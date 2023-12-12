5282cb70ac8349a5301cf240c85dc7277251a195
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-12-12 19:38:25 +0100

    added login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {
@@ -12,7 +13,7 @@ export const routes: Routes = [
         component:RegistrationComponent
     },
     {
-        path:'signin',
-        component:SignInComponent
+        path:'login',
+        component:LoginComponent
     }
 ];
