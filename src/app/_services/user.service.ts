43570a5d18fce5ed61f30ad5d6fc6a5ae917ae93
carlavelasco15 <carlavelasco15@gmail.com>
2023-12-17 12:10:12 +0100

    update details user

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
 
 
 const AUTH_API = 'https://backendworkshelf-production.up.railway.app/';
@@ -12,10 +13,18 @@ const AUTH_API = 'https://backendworkshelf-production.up.railway.app/';
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private tokenService: TokenStorageService) { }
 
   getPublicContent(): Observable<any> {
     return this.http.get(AUTH_API + 'all', { responseType: 'text' });
+  }
+
+  updateUser(id: number, updatedData: any): Observable<any> {
+    var token = this.tokenService.getToken();
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.put<any>(AUTH_API + id, updatedData);
   }
 
   getUserBoard(): Observable<any> {
