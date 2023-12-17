eaad53183d04f39e1bebea3850b2aa58ed736f8b
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-12-17 12:51:09 +0100

    added admin guard + admin panel + notfound page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { UserbookshareComponent } from './components/user/userbookshare/userbook
 import { UseraddbookComponent } from './components/user/useraddbook/useraddbook.component';
 import { UserDetailsComponent } from './components/user/user-details/user-details.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AdminGuard } from './guards/admin.guard';
 
 export const routes: Routes = [
     {
@@ -44,6 +46,15 @@ export const routes: Routes = [
     },
     {
         path:'admin',
+        canActivate : [AdminGuard],
         component:AdminComponent
-    }
+    },
+    {
+        path:'notfound',
+        component:NotFoundComponent
+    },
+    {
+        path:'**',
+        redirectTo: '/notfound'
+    },
 ];
