057a849dbc677ffcd58a9508fa24f564a5301f4f
Marc Pellicer <marcpelli3@gmail.com>
2023-12-18 16:59:47 +0100

    16:59

--- src/app/components/user/userreservations/userreservations.component.ts
+++ src/app/components/user/userreservations/userreservations.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import { ReservationService } from '../../../_services/reservation/reservation.service';
+import { UserService } from '../../../_services/user.service';
+import { TokenStorageService } from '../../../_services/token-storage.service';
+import { DatePipe } from '@angular/common';
+import { Reservation } from '../../../models/reservation/reservation.model';
 
 @Component({
   selector: 'app-userreservations',
   standalone: true,
-  imports: [FooterComponent, RouterLink],
+  imports: [FooterComponent, RouterLink, DatePipe],
   templateUrl: './userreservations.component.html',
   styleUrl: './userreservations.component.css'
 })
-export class UserreservationsComponent {
+export class UserreservationsComponent implements OnInit{
 
+  reservations: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService, private userService: UserService, private tokenStorage: TokenStorageService){}
+
+  ngOnInit(): void {
+
+    const userId = this.tokenStorage.getUser();
+
+    this.reservationService.getReservationsByUser(userId).subscribe(
+      (reservations: any) => {
+        console.log(this.reservations);
+        this.reservations = reservations;
+      },
+      (error) => {
+        console.error('Error fetching user reservations: ', error);
+      }
+    );
+  }
+  
 }
