f712305ed7697e6df8f6accaf3a81a91568901cb
Marc Pellicer <marcpelli3@gmail.com>
2023-12-19 21:11:56 +0100

    almost finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
 import { BookDetailsComponent } from './components/book/book-details/book-details.component';
 import { UserreservationsComponent } from './components/user/userreservations/userreservations.component';
@@ -22,10 +21,6 @@ export const routes: Routes = [
     {
         path:'',
         component:HomeComponent
-    },
-    {
-        path:'registration',
-        component:RegistrationComponent
     },
     {
         path:'login',
