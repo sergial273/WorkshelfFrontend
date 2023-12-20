66782484bd83d2184dc0e8d2aaf83dc651ca3017
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-12-20 12:45:14 +0100

    Update book-details.component.ts

--- src/app/components/book/book-details/book-details.component.ts
+++ src/app/components/book/book-details/book-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { FooterComponent } from '../../shared/footer/footer.component';
 import { Book } from '../../../models/book/book.model';
 import { Rating } from '../../../models/rating/rating.model';
 import { BookserviceService } from '../../../_services/book/bookservice.service';
@@ -10,28 +11,32 @@ import { RatingService } from '../../../_services/rating/rating.service';
 import { ReservationService } from '../../../_services/reservation/reservation.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { FooterComponent } from '../../shared/footer/footer.component';
+import { finalize, switchMap } from 'rxjs/operators';
+import { Reservation } from '../../../models/reservation/reservation.model';
+
 
 @Component({
     selector: 'app-book-details',
     standalone: true,
     templateUrl: './book-details.component.html',
     styleUrl: './book-details.component.css',
-    imports: [FooterComponent, DatePipe, FormsModule, ]
+    imports: [FooterComponent, DatePipe, FormsModule]
 })
 export class BookDetailsComponent implements OnInit {
 
     book: Book = new Book();
     bookId: number = 0;
     bookDetails: any;
-    @Input() ratings: Rating[] = []
-    isReservedByCurrentUser: boolean = true;
+    ratings: Rating[] = []
+    isReservedByCurrentUser: boolean = false;
     isAvailable: boolean = true;
 
     rating: boolean = false; 
     stars: number = 1;
     comment: string = '';
     ratingToAdd: Rating = new Rating();
+
+    reservations: Reservation[] = [];
 
 
     constructor(
@@ -54,22 +59,45 @@ export class BookDetailsComponent implements OnInit {
     }
 
     loadBookDetails(): void {
-        this.bookservice.getBookById(this.bookId).subscribe
-            (data => {
-                this.book = data;
-                //Reserved available = 0 ?
-                this.isAvailable = this.book.reserved === 0;
-                //if (this.tokenStorage.isAuthenticated()) {
-                //    const currentUser = this.tokenStorage.getUser();
-                //    this.isReservedByCurrentUser = this.book.user?.userId === currentUser.id;
-                //}
-            },
-                (error) => {
-                    console.error('Error fetching book details:', error);
-                }
-            );
+      this.bookservice.getBookById(this.bookId).pipe(
+        switchMap((data) => {
+          this.book = data;
+    
+          return this.reservationService.getReservationsByBookNotPaginated(this.book);
+        }),
+        finalize(() => {
+          this.checkIfs();
+        })
+      ).subscribe(
+        (reservations) => {
+          this.reservations = reservations;
+        },
+        (error) => {
+          console.error('Error fetching book details:', error);
+        }
+      );
     }
 
+    findActiveReservation(): Reservation | undefined {
+      const currentDate = new Date();
+      
+      const activeReservation = this.reservations.find(reservation => {
+        return currentDate <= new Date(reservation.returnDate);
+      });
+  
+      return activeReservation;
+    }
+
+    checkIfs(){
+      this.isAvailable = this.book.reserved === 0;
+          if (this.tokenStorage.getToken() !== null ) {
+              const currentUser = this.tokenStorage.getUser();
+              
+              const activeReservation = this.findActiveReservation()
+
+              this.isReservedByCurrentUser = activeReservation?.user.userId === currentUser.userId;
+          }
+    }
     loadRatings(): void {
         this.ratingService.getRatingsByBookId(this.bookId).subscribe(
             (data: Rating[]) => {
@@ -81,10 +109,16 @@ export class BookDetailsComponent implements OnInit {
         );
     }
 
+    getStarArray(score: number): any[] {
+        const fullStars = Math.floor(score);
+        const starArray = Array(fullStars).fill('full-star');
+        
+        return starArray;
+    }
+
     createReservation(book: any) {
         this.reservationService.addReservation(book).subscribe(
           (response) => {
-            console.log('Reserva exitosa:', response);
             window.location.reload();
           },
           (error) => {
@@ -96,7 +130,6 @@ export class BookDetailsComponent implements OnInit {
     returnBook(book: any) {
         this.reservationService.returnBookReservation(book).subscribe(
           (response) => {
-            console.log('Reserva exitosa:', response);
             window.location.reload();
           },
           (error) => {
@@ -115,31 +148,18 @@ export class BookDetailsComponent implements OnInit {
     }
 
     addRating(){
-        this.reservationService.getReserveByUserAndBook(this.book.id).subscribe(
-            (response) => {
-              console.log('Reserva exitosa:', response);
-              this.ratingToAdd.reservation = response
-
-            },
-            (error) => {
-              console.error('Error:', error);
-            }
-          );
-        this.ratingService.addRating(this.ratingToAdd).subscribe(
-            (response) => {
-              console.log('Reserva exitosa:', response);
-              
-            },
-            (error) => {
-              console.error('Error al realizar la reserva:', error);
-            }
-          );
+        this.reservationService.getReserveByUserAndBook(this.book.id).pipe(
+          switchMap((reservation) => {
+            this.ratingToAdd.reservation = reservation;
+        
+            return this.ratingService.addRating(this.ratingToAdd);
+          })
+        ).subscribe(
+          (response) => {
+            window.location.reload();
+          },
+          (error) => {
+            console.error('Error al realizar la reserva:', error);
+          });
     }
-
-    getStarArray(score: number): any[] {
-      const fullStars = Math.floor(score);
-      const starArray = Array(fullStars).fill('full-star');
-      
-      return starArray;
-  }
 }
